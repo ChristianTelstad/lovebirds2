435522caf83079f53242303b7c67f20a109b9b05
Christian Telstad <christiantelstad@Christians-MacBook-Pro.local>
2020-12-01 15:41:14 +0100

    Updated

--- JS/Index.js
+++ JS/Index.js
@@ -36,3 +36,17 @@ function validatePasswordUpperCase() {
 const storageInput = document.querySelector(".storage");
 const text = document.querySelector(".text");
 const button = document.querySelector(".button");
+
+// Create a very simple class
+class User {
+    constructor(firstname, lastname, birthday, gender,email, password, ssn ){
+        this.firstName = firstname;
+        this.lastname = lastname;
+        this.gender = gender;
+        this.birthday = birthday;
+        this.email = email;
+        this.password = password;
+        this.ssn = ssn;
+    }
+}
+
