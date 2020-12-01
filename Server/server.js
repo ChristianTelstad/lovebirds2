4678229c480d0c18fb3121c11b35e5b96a08e30b
Christian Telstad <christiantelstad@Christians-MacBook-Pro.local>
2020-12-01 13:16:58 +0100

    1

---
+++ Server/server.js
@@ -0,0 +1,18 @@
+//gemmer express-library i variabel
+const express = require('express')
+//initialiserer express-server
+const server = express()
+const port = 3000
+
+var path = require("path");
+
+server.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname + "/sign.html"));
+  res
+});
+
+//server aktiveres
+server.listen(port, () => {
+  console.log(`Server-applikation lytter på http://localhost:${port}`)
+})
+
