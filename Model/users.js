b2b2e64d2d7876d36fa6edddee7e6bc6847be960
Christian Telstad <christiantelstad@Christians-MacBook-Pro.local>
2020-12-02 12:21:21 +0100

    Updated

--- Model/users.js
+++ Model/users.js
@@ -152,3 +152,41 @@ fetch('http://localhost:3000/user', {
   console.error('Error:', error);
 });
 }
+
+function validatePasswordLenght() {
+
+    if (password.value.length <= 8) {
+        document.getElementById("passwordLenghtFeedback").style.color = "rgb(248, 191, 191)";
+        event.preventDefault();
+    }
+    else {
+        document.getElementById("passwordLenghtFeedback").style.color = "#bdfcc7";
+    }
+}
+
+function validatePasswordNumber() {
+
+    if (/[0-9]/.test(password.value)) {
+        document.getElementById("passwordNumberFeedback").style.color = "#bdfcc7";
+    }
+    else {
+        document.getElementById("passwordNumberFeedback").style.color = "rgb(248, 191, 191)";
+        event.preventDefault();
+    }  
+}
+
+
+function validatePasswordUpperCase() {
+
+    if (/[A-Z]/.test(password.value)) {
+        document.getElementById("passwordUpperCaseFeedback").style.color = "#bdfcc7";
+    }
+    else {
+        document.getElementById("passwordUpperCaseFeedback").style.color = "rgb(248, 191, 191)";
+        event.preventDefault();
+    }  
+}
+
+const storageInput = document.querySelector(".storage");
+const text = document.querySelector(".text");
+const button = document.querySelector(".button");
