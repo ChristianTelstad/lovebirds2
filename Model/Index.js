9cc467570628b40660310db094d5f66eaf4f366d
Christian Telstad <christiantelstad@Christians-MacBook-Pro.local>
2020-12-02 11:48:18 +0100

    Updated

--- Model/Index.js
+++ Model/Index.js
@@ -37,4 +37,64 @@ const storageInput = document.querySelector(".storage");
 const text = document.querySelector(".text");
 const button = document.querySelector(".button");
 
+class User {
+    constructor(firstname, lastname, birthday, gender,email, password, ssn ){
+        this.firstName = firstname;
+        this.lastname = lastname;
+        this.gender = gender;
+        this.birthday = birthday;
+        this.email = email;
+        this.password = password;
+        this.ssn = ssn;
+    }
+}
 
+// Find the buttom in the DOM
+let button = document.getElementById("submit");
+    
+// We add an event listener
+button.addEventListener("click", function(){
+    let errorText = "";
+    let firstname = document.getElementById("firstname").value;
+    let lastname = document.getElementById("lastname").value;
+    let email = document.getElementById("email").value;
+    let birthday = document.getElementById("birthday").value;
+    let ssn = document.getElementById("ssn").value;
+    let gender = document.getElementById("gender").value;
+    let password = document.getElementById("password").value;
+            
+
+    if(firstname == ""){
+        errorText += "Fistname is empty\n";            
+    }
+    if(lastname == ""){
+        errorText += "Fistname is empty\n";            
+    }
+    /*if(regExForEmail.test(String(email).toLowerCase())){
+        errorText += "Email is not valid\n";
+    }*/
+    if(email == ""){
+        errorText += "email plz"
+    }
+    if(birthday == "") {
+        errorText += "Birthday is not valid or not in valid format\n";
+    }
+    if(ssn == ""){
+        errorText += "Please submit a CPR\n";
+    }
+    if(gender == ""){
+        errorText += "You need to input a valid gender. Either female or male\n";
+    }
+    if(password == ""){
+        errorText += "Please submit password that is at least six chars \n";
+    }
+    
+    // If we have errors we output them
+    if(errorText != ""){
+        document.getElementById("message").innerText = errorText;
+    }else {
+        let newUser = new User(firstname, lastname, birthday, gender, email, password, ssn);
+        //denne funktion er på linje 138
+        uploadUser(newUser)
+    }
+});
