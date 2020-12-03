e1fb8819352b0319d6f6ff603378b6e0341e3a9b
Christian Telstad <christiantelstad@Christians-MacBook-Pro.local>
2020-12-03 12:53:28 +0100

    Updated

--- Server.js
+++ Server.js
@@ -1,7 +1,10 @@
 //gemmer express-library i variabel
 const express = require('express')
+
 //connect to mongoDB
 const mongoose = require("mongoose");
+require('dotenv/config');
+
 //initialiserer express-server
 const server = express()
 const port = 3000
@@ -15,7 +18,7 @@ server.get("/", function(req, res) {
   res.sendFile(path.join(__dirname + "/View/sign.html"))
 });
 
-mongoose.connect('mongodb+srv://ChristianTelstad:Svinndal1@cluster0.oiiky.mongodb.net/Cluster0?retryWrites=true&w=majority', { useNewUrlParser: true}, () => console.log('Connected to DB!'))
+mongoose.connect(process.env.DB_CONNECTION,{ useNewUrlParser: true, useUnifiedTopology: true }, { useUnifiedTopology: true },() => console.log('Connected to DB!'))
 //tester om vi er connected ordentligt 
 mongoose.connection.once('open', function(){ 
   console.log('Connection has been made, get going!');
