9fa2b5410039f80d7dbf03ece8ead20e1c2b7e75
Christian Telstad <christiantelstad@Christians-MacBook-Pro.local>
2020-12-03 12:20:12 +0100

    Updated

--- Server.js
+++ Server.js
@@ -1,5 +1,6 @@
 //gemmer express-library i variabel
 const express = require('express')
+//connect to mongoDB
 const mongoose = require("mongoose");
 //initialiserer express-server
 const server = express()
@@ -14,7 +15,13 @@ server.get("/", function(req, res) {
   res.sendFile(path.join(__dirname + "/View/sign.html"))
 });
 
-mongoose.connect('mongodb+srv://ChristianTelstad:Svinndal1@cluster0.oiiky.mongodb.net/test', { useNewUrlParser: true}, () => console.log('connected to DB!'))
+mongoose.connect('mongodb+srv://ChristianTelstad:Svinndal1@cluster0.oiiky.mongodb.net/Cluster0?retryWrites=true&w=majority', { useNewUrlParser: true}, () => console.log('Connected to DB!'))
+//tester om vi er connected ordentligt 
+mongoose.connection.once('open', function(){ 
+  console.log('Connection has been made, get going!');
+}).on('error', function(error){
+  console.log('Connection error', error)
+});
 
 //server aktiveres
 server.listen(port, () => {
